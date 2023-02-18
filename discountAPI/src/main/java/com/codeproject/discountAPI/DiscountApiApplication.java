1849133a5e9f509d76c6bad8a0d35f28bee9ca67
Sriram Anne <sriramanne@Srirams-MacBook-Pro.local>
2023-02-17 19:22:48 -0500

    base project check in

---
+++ discountAPI/src/main/java/com/codeproject/discountAPI/DiscountApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeproject.discountAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountApiApplication.class, args);
+	}
+
+}
